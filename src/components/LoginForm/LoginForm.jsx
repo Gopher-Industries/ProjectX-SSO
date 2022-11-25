ee46f6c0fe7eb9bb8c4853e4badaddd1bad7f90d
coreybock <admin@coreybock.com>
2022-11-25 16:57:34 +1100

    Added Login Functionality

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 import * as s from "./LoginForm.style";
 import TextInput from "@Components/TextInput/TextInput";
 import Button from "@Components/Button/Button";
+import loadingGif from "@Assets/images/loading.gif";
+
+import {AnimatePresence} from "framer-motion";
+import axios from "axios";
 
 const LoginForm = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleLoading = () => {
+        setIsLoading(!isLoading);
+    }
+
+    const submitLogin = () => {
+        toggleLoading();
+        const user = {
+            username: "",
+            password: ""
+        }
+
+        axios.post(`#`, {user})
+            .then(res => {
+                console.log(res);
+            }).catch(error => {
+                console.log(error.message);
+        });
+    }
+
     return (
-        <s.LoginContainer>
-            <TextInput placeholder={"Login"} type={"text"} id={"form-login"} name={"form-login"} required={true} />
-            <TextInput placeholder={"Password"} type={"password"} id={"form-password"} name={"form-password"} required={true} />
-            <Button name={"form-submit"} id={"form-submit"} />
-        </s.LoginContainer>
+        <AnimatePresence>
+            {!isLoading ? (
+                <s.LoginContainer key={"login-container"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                    <TextInput placeholder={"Login"} type={"text"} id={"form-login"} name={"form-login"} required={true} />
+                    <TextInput placeholder={"Password"} type={"password"} id={"form-password"} name={"form-password"} required={true} />
+                    <Button name={"form-submit"} id={"form-submit"} onClickHandler={submitLogin} />
+                </s.LoginContainer>
+            ) : (
+                <s.LoginLoading key={"login-loading"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{delay:0.5}}>
+                    <img src={loadingGif} alt={"Loading"} />
+                </s.LoginLoading>
+            )}
+        </AnimatePresence>
     );
 };
 
--- src/components/LoginForm/LoginForm.style.jsx
+++ src/components/LoginForm/LoginForm.style.jsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
+import {motion} from "framer-motion";
 
-export const LoginContainer = styled.div`
+export const LoginContainer = styled(motion.div)`
   margin: 20px 0;
   width:270px;
+`
+
+export const LoginLoading = styled(motion.div)`
+  > img {
+    margin-top:20px;
+    width:50px;
+  }
 `
